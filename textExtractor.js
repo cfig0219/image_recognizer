b939245a0554b4ba2e211e1b77ae2f0e79a62bad
cfig0219 <100231908+cfig0219@users.noreply.github.com>
2024-12-18 22:02:14 -0500

    Add files via upload

--- textExtractor.js
+++ textExtractor.js
@@ -9,6 +9,24 @@ export class Extractor {
     
     // Extracts the text from input image
     extractImage(image) {
-		console.log(image);
+		// Extracts the text from the input file
+		Tesseract.recognize(
+            image, // Example image URL
+            'eng',
+            {
+                //logger: (info) => console.log('Progress:', info), // Optional logger
+            }
+        )
+        
+        .then(({ data: { text } }) => {
+            // sets image text equal to extracted text
+            this.imageText = text;
+            console.log('Extracted text:', this.imageText);
+        })
+        
+        // Error message if no text is found
+        .catch((err) => {
+            console.error('Error during test:', err);
+        });
     }
 }
