aad89ecaf179e99a8944162d225e3e12c090b75e
cfig0219 <100231908+cfig0219@users.noreply.github.com>
2024-12-18 20:47:02 -0500

    Add files via upload

---
+++ textExtractor.js
@@ -0,0 +1,14 @@
+export class Extractor {
+    /**
+     * Initializes the text Extractor class
+     */
+    constructor() {
+        // Variable to store extracted text
+        this.imageText = "";
+    }
+    
+    // Extracts the text from input image
+    extractImage(image) {
+		console.log(image);
+    }
+}
